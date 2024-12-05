885d8c89b9a44e9d184704c28dc9fdeafe18ff4d
GisellaAballay <gisellaaballaybonilla@gmail.com>
2024-12-05 19:40:42 +0100

    add get users

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
+import express from "express";
+
+let users = [
+  {id: "1", name: "John Doe", age: "25"},
+  {id: "2", name: "Jane Smith", age: "30"}
+]
+
+const app = express();
+
+// get all users
+app.get("/api/users", (req,res) => {
+  res.json(users);
+})
+
+// get one user by id
+app.get("/api/users/:id", (req,res) => {
+  const id = req.params.id;
+  const user = users.find(user => user.id === id);
+  if(!user) return res.json({ error: 404 });
+  res.json(user);
+})
+
+app.listen(1234, () => {
+  console.log(`Server on http://localhost:1234`);  
+})
+
 process.loadEnvFile();
 const PORT = process.env.PORT
