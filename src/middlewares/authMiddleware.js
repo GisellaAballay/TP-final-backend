3fbc78fb17fd86fc8f85faf67f2c8f6ce1b6f9c5
GisellaAballay <gisellaaballaybonilla@gmail.com>
2024-12-06 11:15:00 +0100

    fixed middleware

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -3,14 +3,14 @@ import jwt from "jsonwebtoken";
 const SECRET_KEY = "PanConManteca";
 
 export const verifyToken = (req, res, next) => {
-  const token = req.headers["authorization"];
+  const token = req.headers["authorization"]?.split(" ")[1];
   if (!token) {
     return res.status(401).json({ error: "Token no proporcionado" });
   }
 
   try {
     const decoded = jwt.verify(token, SECRET_KEY);
-    req.alumnoId = decoded.id;
+    req.userId = decoded.id;
     next();
   } catch (err) {
     res.status(401).json({ error: "Token inválido o expirado" });
