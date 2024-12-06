1e270c5e475e7cc68553832464621c4624c6ddd3
GisellaAballay <gisellaaballaybonilla@gmail.com>
2024-12-06 20:13:18 +0100

    fixed alumnoController

--- src/controllers/alumnoController.js
+++ src/controllers/alumnoController.js
@@ -1,5 +1,6 @@
-let alumnos = [];
-let turnos = [
+import Alumno from "../models/alumnoModels";
+
+const turnos = [
   {id: "1", name: "Técnica", hours: "Lunes y Miercoes: 20hs"},
   {id: "2", name: "Jazz", hours: "Martes, Jueves y Viernes: 19hs"},
   {id: "3", name: "Heels", hours: "Lunes y Miercoles: 18hs"},
@@ -8,26 +9,41 @@ let turnos = [
   {id: "6", name: "Flexi", hours: "Lunes, Miercoles y Viernes: 17hs"},
 ];
 
-export const getMe = (req, res) => {
-  const alumno = alumnos.find((a) => a.id === req.alumnoId);
-  if (!alumno) return res.status(404).json({ error: "Alumno no encontrado" });
-
-  res.status(200).json(alumno);
-};
-
-export const updateTurnos = (req, res) => {
-  const { turnosSeleccionados } = req.body;
-
-  const alumno = alumnos.find((a) => a.id === req.alumnoId);
-  if (!alumno) return res.status(404).json({ error: "Alumno no encontrado" });
-
-  const turnosValidos = turnosSeleccionados.every((id) =>
-    turnos.some((t) => t.id === id)
-  );
-  if (!turnosValidos) {
-    return res.status(400).json({ error: "Algunos turnos no son válidos" });
+export const getMe = async (req, res) => {
+  try {
+    const alumno = await Alumno.findById(req.alumnoId);
+    if (!alumno) {
+      return res.status(404).json({ error: "Alumno no encontrado" });
+    }
+    res.status(200).json(alumno);
+  } catch (error) {
+    res.status(500).json({ error: "Error al obtener el alumno" });
   }
-
-  alumno.turnosSeleccionados = turnosSeleccionados;
-  res.status(200).json({ message: "Turnos actualizados con éxito", turnosSeleccionados });
 };
+
+export const updateTurnos = async (req, res) => {
+  try {
+    const { turnosSeleccionados } = req.body;
+    const turnosValidos = turnosSeleccionados.every((id) =>
+      turnos.some((t) => t.id === id)
+    );
+    if (!turnosValidos) {
+      return res.status(400).json({ error: "Algunos turnos no son válidos" });
+    }
+    const alumno = await Alumno.findByIdAndUpdate(
+      req.alumnoId,
+      { turnosSeleccionados },
+      { new: true }
+    );
+    if (!alumno) {
+      return res.status(404).json({ error: "Alumno no encontrado" });
+    };
+    res.status(200).json({
+      message: "Turnos actualizados con éxito",
+      turnosSeleccionados: alumno.turnosSeleccionados,
+    });
+  } catch (error) {
+    res.status(500).json({ error: "Error al actualizar los turnos" });
+  }
+};
+
