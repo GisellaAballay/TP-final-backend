686ea8960f29a24f544671526a3b8a5b7f506c01
GisellaAballay <gisellaaballaybonilla@gmail.com>
2024-12-06 19:28:52 +0100

    add schema

--- src/models/alumnoModels.js
+++ src/models/alumnoModels.js
@@ -1,2 +1,12 @@
 // Manipulación de la base de datos
 // Retorna esta petición en la base de dtos al controller
+
+import mongoose from "mongoose";
+
+// definir schema de la entidad
+const alumnoSchema = new mongoose.Schema({
+  name: {type: String},
+  email: {type: email},
+  horario: {type: String}
+});
+
